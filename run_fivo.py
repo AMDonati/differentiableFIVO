902dd71ec40d2cba738c64000e8aaf9b27428aff
AMDonati <alice.martindonati@gmail.com>
2020-09-22 15:54:41 +0200

    back to multinomial resampling

--- run_fivo.py
+++ run_fivo.py
@@ -73,7 +73,7 @@ tf.app.flags.DEFINE_integer("max_steps", int(1e5),
                             "The number of gradient update steps to train for.")  # TODO: import param
 tf.app.flags.DEFINE_integer("summarize_every", 50,
                             "The number of steps between summaries.")
-tf.app.flags.DEFINE_enum("resampling_type", "differentiable",
+tf.app.flags.DEFINE_enum("resampling_type", "multinomial",
                          ["multinomial", "relaxed", "differentiable"],
                          "The resampling strategy to use for training.")
 tf.app.flags.DEFINE_float("sinkhorn_regularization", 0.1,
