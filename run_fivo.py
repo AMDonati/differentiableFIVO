07c51dfbd3a85b194f260ed3bca63b69f88f091b
AMDonati <alice.martindonati@gmail.com>
2020-09-24 09:33:41 +0200

    hparams run_fivo

--- run_fivo.py
+++ run_fivo.py
@@ -69,7 +69,7 @@ tf.app.flags.DEFINE_boolean("standardize", False,
                             "standardize input data.")
 tf.app.flags.DEFINE_float("learning_rate", 3e-5,
                           "The learning rate for ADAM.")  # TODO: import param
-tf.app.flags.DEFINE_integer("max_steps", int(1e5),
+tf.app.flags.DEFINE_integer("max_steps", int(5e4),
                             "The number of gradient update steps to train for.")  # TODO: import param
 tf.app.flags.DEFINE_integer("summarize_every", 50,
                             "The number of steps between summaries.")
